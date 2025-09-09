873145b7170f3d21f28c583f5c94e9c874ba40c3
MiriamGarfunkel <Hand1e5@mbjcomp.org.il>
2025-09-10 01:03:56 +0300

    adding

---
+++ HomeWorkLesson1/HomeWork1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes;
+namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle[] c=new Circle[10];
+            var Circle1 = from Circle in c
+                          where Circle.radious > 3
+                          select Circle;
+
+        }
+    }
+}
---
+++ HomeWorkLesson1/Shapes/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle
+    {
+        public int radious { get; set; }
+        public int size { get; set; }
+    }
+}
