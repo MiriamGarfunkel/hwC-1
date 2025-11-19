8c95ee0419f1ea95107a636df2ad78ab7d885ee7
MiriamGarfunkel <Hand1e5@mbjcomp.org.il>
2025-11-19 17:04:18 +0200

    Add files via upload

---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lesson7;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var manager = new Calls("..\\..\\..\\..\\phone.xml");
+
+        var miriamCalls = manager.func1();
+        Console.WriteLine("length of calls to miriam from phone 1");
+        miriamCalls.ForEach(Console.WriteLine);
+
+        var shiraPhones = manager.func2();
+        Console.WriteLine("\nphones that somone spoke to shira:");
+        shiraPhones.ForEach(Console.WriteLine);
+
+        var longCalls = manager.func3();
+        Console.WriteLine("\nphones that spoke over 10 min");
+        longCalls.ForEach(Console.WriteLine);
+
+        manager.AddCall("2", "09:44", "40", "chaya");
+        manager.DeleteCall("1", "dina");
+    }
+}
+
+
+
+
